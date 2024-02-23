87a1cbc017a064fb42a311307c647cd37dd9db4c
goutham <uppinurigouthamreddy@gmail.com>
2024-02-23 19:14:31 +0530

    cros policy modified 1

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const exp=require('express')
 const { stderr } = require('process')
 const app=exp()
 const {exec} = require('child_process')
+const cors=require('cors')
 
 require("dotenv").config()
 
 app.use(exp.json())
+app.use(cors())
 
 app.get('',(req,res)=>{
     res.send({message:"hello, welcome to OTP generator API, its free to use , to acces this resource Contact : UPPUNURI GOUTHAM REDDY ,IT-C,VNR VJIET"})
